a6e68ffa0504f5fad6547219cbcefbf0282cbf5d
Mbo1a <francinerazafy2003@gmail.com>
2022-07-23 11:35:24 +0200

    git tuto

--- hello.java
+++ hello.java
@@ -1,5 +1,14 @@
 class hello {
     public static void main(String[] args){
         System.out.println("Hello World");
+
+        int a = 4; int b = 19;
+        int c = a + b;
+        System.out.println("Somme de " + a + " et " + b + " est " + c);
+
+        for(int i=0;i<5; i++) {
+            System.out.println("I Love Mbola");
+            
+        }
     }
 }
